ace6f54d0efd096dae65f61b29563abc7b92d240
Josef Rousek <josef.rousek@gmail.com>
2018-02-26 09:18:15 +0100

    Add docs

---
+++ README.md
@@ -0,0 +1,29 @@
+# GraphQL Kiwi flight search
+
+Simple flight search tool build using Next.js and styled-components.
+
+## Installation
+
+```
+yarn
+```
+
+or
+
+```
+npm install
+```
+
+## Development
+
+Start the server.
+
+```sh
+yarn dev
+```
+
+### Tests
+
+```sh
+yarn test
+```
--- pages/about.js
+++ pages/about.js
@@ -2,5 +2,9 @@ import Layout from '../components/Layout';
 
 export default () =>
   <Layout>
-    <p>This is the about page</p>
+    <p>Simple flight search tool build using Next.js and styled-components.</p>
+    <p>
+      You can see the source code on{' '}
+      <a href="https://github.com/stlk/flight-search">Github.com</a>.
+    </p>
   </Layout>;
